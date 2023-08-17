86c7e05eca98301dbf05ffe8444445a2c7388517
TranQuocHuuSOS <0tranquochuu02003@gmail.com>
2023-08-17 17:01:28 +0700

    typeScript get Api

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+interface Pokemons{
+  name:string;
+  url:string;
+}
+interface Pokemon{
+  id:number;
+  name:string;
+  prites:{
+    front_default:string;
+  }
+}
+const App:React.FC=(url)=> {
+  const [pokemons, setPokemons]= useState<Pokemon[]>([]);
+  useEffect(() => {
+    const getPokemon = async () => {
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
+      );
+      
+      res.data.results.forEach(async (pokemon: Pokemons) => {
+        const poke = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        setPokemons((p) => [...p, poke.data]);
+        ;
+      });
+    };
+    getPokemon();
+  }, []);
+  return (
+    <div className="App">
+        <div className='container'>
+          <header className='pokemon-header'>
+            Pokemon
+          </header>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PokemonColection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PokemonColection = () => {
+  return (
+    <div>PokemonColection</div>
+  )
+}
